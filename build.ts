7bdeed4c99616bfc4f2a8648acec7323eeb7f253
Chirayu Chhabra <chirayuchhabra4@gmail.com>
2026-06-27 02:31:17 +0530

    chore: update build.ts

--- build.ts
+++ build.ts
@@ -2,7 +2,7 @@ import { rmSync, cpSync, mkdirSync, existsSync } from "node:fs";
 import { join } from "node:path";
 import { $ } from "bun";
 
-console.log("Building SDK...");
+console.log("Building framework...");
 if (existsSync("dist")) {
   rmSync("dist", { recursive: true, force: true });
 }
@@ -12,7 +12,7 @@ cpSync("src/styles", "dist/styles", { recursive: true });
 cpSync("src/client", "dist/client", { recursive: true });
 
 await Bun.build({
-  entrypoints: ["./src/client/app.js"],
+  entrypoints: ["./src/client/app.ts"],
   outdir: "./dist/client",
   naming: "[dir]/app.bundle.[ext]",
   minify: true,
